178e5404cb6f8d512ffd445f1a17960c4b9e0909
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-12-02 10:12:11 -0600

    implementing pep8 style guidelines

--- collect_links.py
+++ collect_links.py
@@ -4,7 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
 def collect_links():
     """ Collect all the EDGAR links that have not yet been parsed
 
@@ -14,22 +13,24 @@ def collect_links():
     headers = {
         "User-Agent": "Carlo Tran carlotran4@gmail.com"
     }
-    page = requests.get(url, headers=headers,timeout = 30)
+    page = requests.get(url, headers=headers, timeout=30)
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    #Get all Acc No's
+    # Get all Acc No's
     acc_no = []
     for i in soup.find_all(string=re.compile("Accession Number:")):
-        i = i.replace("Accession Number: ","")
+        i = i.replace("Accession Number: ", "")
         i = i.split()[0]
         acc_no.append(i)
 
     cik = []
-    #Get all CIK's
+    # Get all CIK's
     for i in soup.find_all("a"):
         i = str(i)
-        if "cgi-bin" in i and  "file" not in i and "getcurrent" not in i:
+        if "cgi-bin" in i and "file" not in i and "getcurrent" not in i:
             cik.append(i[57:67])
-    print(cik,acc_no,sep = "\n")
+    print(cik, acc_no, sep="\n")
+
+
 collect_links()
